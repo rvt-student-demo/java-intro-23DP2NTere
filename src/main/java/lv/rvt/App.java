45dab8bb6eb91ec09c6d4f3267483e2f25c11978
23DP2NTere <166606051+23DP2NTere@users.noreply.github.com>
2024-09-05 09:50:16 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,24 @@
-package lv.rvt;
+package lv.rvt; // Klases atrašanas vieta projektā
+
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ievadi vārdu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadiet uzvārdu: ");
+        String surname = scanner.nextLine();
+
+        System.out.println("Ievadiet grupu: ");
+        String group = scanner.nextLine();
+    
+
+        System.out.println(name + " " + surname + " " + group);
     }
 
 }
