fd437592ab707d9c370fe94676913fd91a8df8cd
23DP2NTere <166606051+23DP2NTere@users.noreply.github.com>
2024-11-06 11:05:16 +0000

    Decreasing Counter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,25 +5,10 @@ import java.util.Scanner;
 public class App {
     
     public static void main(String[] args) {
-
-        DecreasingCounter counter = new DecreasingCounter(100);
-
-        counter.printValue();
-
-        counter.decrement();
-        counter.printValue();
-
-        counter.decrement();
-        counter.printValue();
-
-        counter.reset();
-        counter.printValue();
-
-        counter.decrement();
-        counter.printValue();
-    }
+        
 
     }
+}
 
 
 
