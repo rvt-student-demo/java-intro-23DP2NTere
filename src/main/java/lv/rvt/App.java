0204a8ab948297522bf60dc39bf83c62593a59e8
23DP2NTere <166606051+23DP2NTere@users.noreply.github.com>
2024-11-07 09:47:03 +0000

    Statistics

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,14 +6,22 @@ public class App {
     
     public static void main(String[] args) {
     
-    
+        Scanner scanner = new Scanner(System.in);
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
 
+        int number = 0;
+        System.out.println("Enter numbers: ");
+        while (number != -1) {
+            number = scanner.nextInt();
+            statistics.addNumber(number);
+
+            //statistics.addNumber(2);
+        }
+
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
+        
     }
 }
 
--- src/main/java/lv/rvt/Statistics.java
+++ src/main/java/lv/rvt/Statistics.java
@@ -1,25 +1,33 @@
 package lv.rvt;
 
 public class Statistics {
-   
     private int count;
+    private int sum;
+
 
     public Statistics() {
         // initialize the variable numberCount here
-        int numberCount;
+        this.count = 0;
+        this.sum = 0;
     }
 
     public void addNumber(int number) {
-
+        // write code here
+        this.count++;
+        sum += number;
     }
 
     public int getCount() {
         // write code here
-        
+        return count;
     }
 
     public int sum() {
-
+        return sum;
     }
+
+    public double average() {
+        return (double) sum / count;
+    } 
 }
 
