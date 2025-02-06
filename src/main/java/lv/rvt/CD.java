bba544f2f965a34d5159519bad71785951bdfbdf
23DP2NTere <166606051+23DP2NTere@users.noreply.github.com>
2025-02-06 11:28:20 +0000

    Interface In A Box

---
+++ src/main/java/lv/rvt/CD.java
@@ -0,0 +1,18 @@
+package lv.rvt;
+
+public class CD implements Packable {
+    private String artist;
+    private String name;
+    private int year;
+
+    public CD(String artist, String name, int year) {
+        this.artist = artist;
+        this.name = name;
+        this.year = year;
+    }
+
+    @Override
+    public double weight() {
+        return 0.1;
+    }
+}
